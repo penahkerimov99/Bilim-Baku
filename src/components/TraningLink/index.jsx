6d6c4b8b984a4c8f85a6840a619a41dd5a863a3a
cavidanehmedov <104515083+cavidanehmedov@users.noreply.github.com>
2022-08-14 13:26:14 +0400

    Bir daha yoxlayin

--- src/components/TraningLink/index.jsx
+++ src/components/TraningLink/index.jsx
@@ -4,7 +4,7 @@ import './style.scss'
 export const TraningLink = ({link1, link2}) => {
   return (<div className='traning-links'>
     <a className='link1'>{link1}</a> 
-    <a className='link2' href=""><i class={link2}>Narrow</i></a> 
+    <a className='link2' href=""><i class={link2}>NARROW</i></a> 
    </div> 
   )
 }
