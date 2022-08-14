794fe8725bf4bc805b1de977c67ea0dd8442bb9a
penahkerimov99 <104515090+penahkerimov99@users.noreply.github.com>
2022-08-14 18:14:36 +0400

    section1 is complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import {Main} from './components/Main'
 import data from './data/data.json'
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
