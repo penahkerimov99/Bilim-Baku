793140716c61f013f55ee1870c13d9e786a5c5c4
penahkerimov99 <104515090+penahkerimov99@users.noreply.github.com>
2022-08-15 11:05:26 +0400

    scroll is deleted

--- src/components/Navitems/index.jsx
+++ src/components/Navitems/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import NavButtons from './Navbuttons'
 import Language from './Languages'
-import NavButton from './Navbutton'
 import './style.scss'
+import { Button } from '../shared/Button'
 
 const NavItems = () => {
   return (
     <div className='nav-items'>
     <NavButtons/>
     <Language/>
-    <NavButton/>
+    <Button/>
+    
     </div>
   )
 }
